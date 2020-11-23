a9d7f07d8441c0b0883b2270a5508f93cf155f32
ryohei0419 <ryoheiprograming@gmail.com>
2020-11-23 23:48:47 +0900

    Add Procfile + varchar = 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,7 +17,6 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    use Illuminate\Support\Facades\Schema;
     /**
      * Bootstrap any application services.
      *
