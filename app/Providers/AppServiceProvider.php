472dfd3a9fff78fda2ba8539bdc7a1f5bd20d1bb
ryohei0419 <ryoheiprograming@gmail.com>
2020-11-23 23:47:08 +0900

    Add Procfile + varchar = 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
+    use Illuminate\Support\Facades\Schema;
     /**
      * Bootstrap any application services.
      *
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
+        Schema::defaultStringLength(191);
     }
 }
